01ab32cd0467447d3a6db500a68e7bf238c2687b
pancakeslmao <phucthin29@gmail.com>
2025-04-02 14:26:35 +0700

    refactor items 2

--- app/src/main/java/com/example/projectmanagement/ui/notification/NotificationViewModel.kt
+++ app/src/main/java/com/example/projectmanagement/ui/notification/NotificationViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is notification Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/projectmanagement/ui/projects/ProjectsViewModel.kt
+++ app/src/main/java/com/example/projectmanagement/ui/projects/ProjectsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProjectsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is projects Fragment"
     }
     val text: LiveData<String> = _text
 }
