304804eb5d4f007f11f8a0f678305dd3705295cf
pancakeslmao <phucthin29@gmail.com>
2025-04-06 12:51:26 +0700

    Fixed: padding layout and color

--- app/src/main/java/com/example/projectmanagement/CreateProjectActivity.kt
+++ app/src/main/java/com/example/projectmanagement/CreateProjectActivity.kt
@@ -12,16 +12,15 @@ class CreateProjectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         binding = ActivityCreateProjectBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
         binding.backBtn.setOnClickListener {
             finish()
         }
